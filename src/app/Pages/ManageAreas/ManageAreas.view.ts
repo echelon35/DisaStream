a4ef0006f5e721aa7122dc2177bd91082a25ae94
Kevin brun <kbrun.pro@gmail.com>
2024-09-23 18:10:13 +0200

    Begin dashboard

---
+++ src/app/Pages/ManageAreas/ManageAreas.view.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: './ManageAreas.view.html',
+  styleUrls: ['./ManageAreas.view.css']
+})
+export class ManageAreasView {
+  title = 'Connectez-vous aux forces de la nature avec Cataclysm';
+}
