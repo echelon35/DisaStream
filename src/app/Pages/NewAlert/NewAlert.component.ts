e4c6ab29c655095222f900cecc0ca1ae50403893
echelon35 <kbrun.pro@gmail.com>
2024-11-19 21:58:06 +0100

    Badge alea

--- src/app/Pages/NewAlert/NewAlert.component.ts
+++ src/app/Pages/NewAlert/NewAlert.component.ts
@@ -8,7 +8,6 @@ import { Alea } from "src/app/Model/Alea";
 import { Alert } from "src/app/Model/Alert";
 import { MailAlert } from "src/app/Model/MailAlert";
 import { AlertApiService } from "src/app/Services/AlertApiService";
-import { Step } from "src/app/Shared/Components/Stepper/Stepper.component";
 import { ElementRef } from '@angular/core';
 import { ToastrService } from "src/app/Shared/Services/toastr.service";
 
@@ -29,60 +28,29 @@ export class NewAlertView {
     selectedLayer?: L.GeoJSON;
     public locationBox?: L.LatLngBounds;
 
-    public steps: Step[] = [{
-      stepLabel: 'Où ?',
-      click: () => { this.changeStep('where-step') },
-      stepId: 'where-step',
-      isActive: true,
-      passed: false,
-      iconPath: '/assets/images/svg/draw_area.svg'
-    },
-    {
-      stepLabel: 'Quoi ?',
-      click: () => { this.changeStep('alea-step') },
-      stepId: 'alea-step',
-      passed: false,
-      isActive: false,
-            iconPath: '/assets/images/svg/alea_choice.svg'
-    },{
-      stepLabel: 'Qui ?',
-      click: () => { this.changeStep('mail-step') },
-      stepId: 'mail-step',
-      passed: false,
-      isActive: false,
-            iconPath: '/assets/images/svg/mails_to_contact.svg'
-    },{
-      stepLabel: 'Terminer',
-      click: () => { this.changeStep('final-step') },
-      isActive: false,
-      iconPath: '/assets/images/svg/final_step.svg',
-      stepId: 'final-step',
-      passed: false,
-    }]
-
     formGroup = this._formBuilder.group({
       name: ['', Validators.required],
     });
 
     constructor(private alertApiService: AlertApiService, private router: Router, private route: ActivatedRoute, private toastrService: ToastrService, private elementRef: ElementRef<HTMLElement>){
-      if(this.route.snapshot.queryParamMap.get('id') != null){
-        const id = parseInt(this.route.snapshot.queryParamMap.get('id')!);
-        console.log(id);
-        this.alertApiService.getAlertById(id).subscribe({
-          next: (alert) => {
-            this.loadedAlert.next(alert);
-            this.alert = alert;
-          },
-          error: (error) => { 
-            if(error.status == 403){
-              this.router.navigateByUrl('dashboard/alerts/manage').then(() => { this.toastrService.error('Vous n\'êtes pas autorisé à accéder à cette alerte'); 
-            })}
-            else {
-              this.toastrService.error(error?.message);
-            }
-          }
-        })
-      }
+      // if(this.route.snapshot.queryParamMap.get('id') != null){
+      //   const id = parseInt(this.route.snapshot.queryParamMap.get('id')!);
+      //   console.log(id);
+      //   this.alertApiService.getAlertById(id).subscribe({
+      //     next: (alert) => {
+      //       this.loadedAlert.next(alert);
+      //       this.alert = alert;
+      //     },
+      //     error: (error) => { 
+      //       if(error.status == 403){
+      //         this.router.navigateByUrl('dashboard/alerts/manage').then(() => { this.toastrService.error('Vous n\'êtes pas autorisé à accéder à cette alerte'); 
+      //       })}
+      //       else {
+      //         this.toastrService.error(error?.message);
+      //       }
+      //     }
+      //   })
+      // }
     }
 
     showPanel(){
@@ -115,23 +83,6 @@ export class NewAlertView {
     }, 5);
 }
 
-    changeStep(nextStep: string){
-      const activeStep = this.steps.find(item => item.isActive);
-      console.log(activeStep?.stepId);
-      console.log(nextStep);
-      if(activeStep?.stepId == nextStep){
-        return;
-      }
-      if(activeStep){
-        activeStep.passed = true;
-        activeStep.isActive = false;
-        const element = document.getElementById(activeStep.stepId);
-        const nextElement = document.getElementById(nextStep);
-        this.fade(element!);
-        this.unfade(nextElement!);
-      }
-    }
-
     /**
      * First step -> Get areas
      * @param layer 
@@ -145,15 +96,6 @@ export class NewAlertView {
         this.alert.areas = null;
       }
       console.log(this.alert.areas);
-    }
-
-    //Step completed by subform
-    completeStep(nextStep: string){
-      const step = this.steps.find(item => item.stepId == nextStep);
-      step!.click();
-      this.steps.forEach(item => item.isActive = false);
-      step!.isActive = true;
-      this.changeStep(nextStep);
     }
 
     addMails(mails: MailAlert[]){
