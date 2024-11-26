0dafb7eeead10a05e5a45d94ac5c19108483a0c8
Kevin brun <kbrun.pro@gmail.com>
2024-11-26 15:59:58 +0100

    Display error message after google login

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,6 +4,7 @@ export const environment = {
     settings:{
       google_key: "1048039478234-pemeeikgqkj566n580hgv6227vrkvfn7.apps.googleusercontent.com",
       backend: "https://disastream.com/api",
+      disasterapi: "https://disastream.com/disasterapi",
       version: "1.0.0",
       appName: "Disastream"
     }
