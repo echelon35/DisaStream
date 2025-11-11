fc495c0069a751b9485366cb29dd0db0d67e1d9a
echelon35 <kbrun.pro@gmail.com>
2025-11-11 10:06:29 +0100

    Fix 404 picture

--- src/app/Pages/PageNotFoundView/PageNotFound.view.ts
+++ src/app/Pages/PageNotFoundView/PageNotFound.view.ts
@@ -1,11 +1,15 @@
 
 import { Component } from '@angular/core';
 import { SeoService } from 'src/app/Services/Seo.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   templateUrl: './PageNotFound.view.html',
 })
 export class PageNotFoundView {
+
+  #env = environment;
+  protected s3BasePath = this.#env.settings.s3_bucket;
 
   constructor(private seoService: SeoService) { 
     this.seoService.generateTags("Oups, page non trouvée !","La page que vous cherchez n'est plus sur Disastream","/assets/background/404.jpg");
