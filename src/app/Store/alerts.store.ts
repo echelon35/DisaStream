a96d2824833d067da071ec83a35dfb669b4f7b08
echelon35 <kbrun.pro@gmail.com>
2026-02-14 11:36:11 +0100

    Alerts Store

---
+++ src/app/Store/alerts.store.ts
@@ -0,0 +1,76 @@
+import { patchState, signalStore, withHooks, withMethods, withProps, withState } from '@ngrx/signals';
+import { inject } from '@angular/core';
+import { AlertApiService } from 'src/app/Services/AlertApiService';
+import { AlertVm } from 'src/app/Pages/DisasterView/disaster.view';
+import { GeographyApiService } from 'src/app/Services/GeographyApi.service';
+import { MarkerService } from 'src/app/Map/Services/marker.service';
+import { catchError, forkJoin, map, of, switchMap } from 'rxjs';
+
+type LoadAlertsState = {
+  alerts: AlertVm[];
+  isLoading: boolean;
+  error: string | null;
+};
+
+const initialState: LoadAlertsState = {
+  alerts: [],
+  isLoading: false,
+  error: null,
+};
+
+export const AlertsStore = signalStore(
+    {providedIn: 'root'},
+    withState(initialState),
+    withProps(() => ({
+        alertApiService: inject(AlertApiService),
+        geographyService: inject(GeographyApiService),
+        markerService: inject(MarkerService),
+    })),
+    withMethods(({ alertApiService, geographyService, markerService, ...store }) => ({
+        loadAlerts: () => {
+            patchState(store, { isLoading: true });
+            alertApiService.getUserAlerts().pipe(
+                switchMap(alerts => {
+                    const alertWithCountry = alerts.map(alert => {
+                        console.log("Processing alert : ", alert);
+                        if(!alert.countryId){
+                            return of({ alert, country: null });
+                        }
+                        return geographyService.getCountryById(alert.countryId).pipe(
+                            map(country => ({ alert, country })),
+                            catchError(() => of({ alert, country: null }))
+                        );
+                    });
+                    return forkJoin(alertWithCountry);
+                })
+            ).subscribe({
+                next: (alertsWithCountry) => {
+                    const alertVms = alertsWithCountry.map((a => {
+                        const alertVm = new AlertVm();
+                        alertVm.alert = a.alert;
+                        alertVm.country = a.country;
+                        alertVm.visible = true;
+                        return alertVm;
+                    }));
+                    patchState(store, {
+                        alerts: alertVms,
+                        isLoading: false,
+                        error: null,
+                    });
+                    console.log("Alerts loaded in store : ", alertVms);
+                },
+                error: (error) => {
+                    patchState(store, {
+                        isLoading: false,
+                        error: error.message || 'Unknown error',
+                    });
+                },
+            })
+        }
+    })),
+    withHooks({
+        onInit: ({ loadAlerts, ...store }) => {
+             patchState(store, { isLoading: true });
+        }
+    })
+);
