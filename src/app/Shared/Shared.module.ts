f3a8d0d1774e3fba0487f8d9380cc537638a3afa
echelon35 <kbrun.pro@gmail.com>
2026-02-15 21:23:35 +0100

    Remove stepper (useless)

--- src/app/Shared/Shared.module.ts
+++ src/app/Shared/Shared.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { Footer } from "./Components/Footer/Footer.component";
 import { RouterModule } from "@angular/router";
-import { StepperComponent } from "./Components/Stepper/Stepper.component";
 import { ToastrComponent } from "./Components/Toastr/Toastr.component";
 import { SpinnerComponent } from "./Components/Spinner/Spinner.component";
 import { Pagination } from "./Components/Pagination/Pagination.component";
@@ -11,11 +10,10 @@ import { Pagination } from "./Components/Pagination/Pagination.component";
   declarations: [
     Footer,
     ToastrComponent,
-    StepperComponent,
     SpinnerComponent,
     Pagination
   ],
   imports: [CommonModule, RouterModule],
-  exports: [Footer, StepperComponent, ToastrComponent, SpinnerComponent, Pagination]
+  exports: [Footer, ToastrComponent, SpinnerComponent, Pagination]
 })
 export class SharedModule { }
